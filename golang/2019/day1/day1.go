41f36b3acced7db43acc3bc30010ec2bc3b7624b
Andreas Lundgren <andreas@lundgren.io>
2019-12-01 20:33:35 +0100

    more golang

--- golang/2019/day1/day1.go
+++ golang/2019/day1/day1.go
@@ -4,12 +4,24 @@ import (
 	"aoc/aoc"
 )
 
-var CorrectAnswers = []string{"", ""}
+var CorrectAnswers = []string{"3481005", "5218616"}
 
 var Examples = []aoc.Example{
-	{Input: "", Expected1: "", Expected2: ""},
+	{Input: "14", Expected1: "2", Expected2: "2"},
+	{Input: "1969", Expected1: "654", Expected2: "966"},
+	{Input: "100756", Expected1: "33583", Expected2: "50346"},
 }
 
 func Solve(in aoc.Input) (interface{}, interface{}) {
-	return "", ""
+	part1, part2 := 0, 0
+	for _, mass := range in.IntList() {
+		fuel := (mass / 3) - 2
+		part1 += fuel
+		for fuel > 0 {
+			part2 += fuel
+			fuel = (fuel / 3) - 2
+		}
+	}
+
+	return part1, part2
 }
