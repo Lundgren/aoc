5198ae1c77ac558877380375a42c9b3eba799930
Andreas Lundgren <andreasl@opay.team>
2019-12-03 08:27:58 +0100

    Day 3

--- golang/2019/day3/day3.go
+++ golang/2019/day3/day3.go
@@ -2,14 +2,60 @@ package day3
 
 import (
 	"aoc/aoc"
+	"strings"
 )
 
 var CorrectAnswers = []string{"", ""}
 
 var Examples = []aoc.Example{
-	{Input: "", Expected1: "", Expected2: ""},
+	{Input: `R8,U5,L5,D3
+U7,R6,D4,L4`, Expected1: "6", Expected2: "30"},
+	{Input: `R75,D30,R83,U83,L12,D49,R71,U7,L72
+U62,R66,U55,R34,D71,R55,D58,R83`, Expected1: "159", Expected2: "610"},
+	{Input: `R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+U98,R91,D20,R16,D67,R40,U7,R15,U6,R7`, Expected1: "135", Expected2: "410"},
 }
 
 func Solve(in aoc.Input) (interface{}, interface{}) {
-	return "", ""
+	part1, part2 := aoc.NewMinMaxer(), aoc.NewMinMaxer()
+
+	l := in.Lines()
+	p1, p2 := move(l[0]), move(l[1])
+	for pos := range p1.Visits {
+		if _, ok := p2.Visits[pos]; ok && pos != "[0, 0]" {
+			part1.Register(aoc.NewPointFromString(pos).DistanceFrom(0, 0))
+		}
+	}
+	for pos := range p1.Visits {
+		if _, ok := p2.Visits[pos]; ok && pos != "[0, 0]" {
+			part2.Register(p1.MovesTo[pos] + p2.MovesTo[pos])
+		}
+	}
+
+	return part1.Min, part2.Min
+}
+
+func move(instructions string) *aoc.Point {
+	pt := aoc.NewPoint()
+
+	for _, instr := range strings.Split(instructions, ",") {
+		if instr != "" {
+			dir := instr[0]
+			dist := aoc.ParseInt(instr[1:])
+			for i := 0; i < dist; i++ {
+				switch dir {
+				case 'U':
+					pt.MoveNorth()
+				case 'D':
+					pt.MoveSouth()
+				case 'R':
+					pt.MoveEast()
+				case 'L':
+					pt.MoveWest()
+				}
+			}
+		}
+	}
+
+	return pt
 }
--- golang/aoc/point.go
+++ golang/aoc/point.go
@@ -2,6 +2,7 @@ package aoc
 
 import (
 	"fmt"
+	"strings"
 )
 
 const (
@@ -12,18 +13,20 @@ const (
 )
 
 type Point struct {
-	X, Y      int
-	Limits    []int
-	Direction int
-	Visits    map[string]int
+	X, Y                   int
+	Move                   int
+	MinX, MaxX, MinY, MaxY int
+	Limits                 []int
+	Direction              int
+	Visits                 map[string]int
+	MovesTo                map[string]int
 }
 
 func NewPoint() *Point {
 	return &Point{
-		X:         0,
-		Y:         0,
 		Direction: north,
 		Visits:    map[string]int{"[0, 0]": 1},
+		MovesTo:   map[string]int{"[0, 0]": 0},
 	}
 }
 
@@ -33,8 +36,13 @@ func NewPointFromString(p string) *Point {
 	return &Point{
 		X:         x,
 		Y:         y,
+		MaxX:      x,
+		MinX:      x,
+		MaxY:      y,
+		MinY:      y,
 		Direction: north,
 		Visits:    map[string]int{fmt.Sprintf("[%d, %d]", x, y): 1},
+		MovesTo:   map[string]int{fmt.Sprintf("[%d, %d]", x, y): 0},
 	}
 }
 
@@ -92,9 +100,35 @@ func (p *Point) BeenBefore() bool {
 }
 
 func (p *Point) registerVisit() {
+	p.Move++
 	p.Visits[p.String()]++
+	p.MovesTo[p.String()] = p.Move
+	p.MaxX = Max(p.X, p.MaxX)
+	p.MinX = Min(p.X, p.MinX)
+	p.MaxY = Max(p.Y, p.MaxY)
+	p.MinY = Min(p.Y, p.MinY)
 }
 
 func (p *Point) String() string {
 	return fmt.Sprintf("[%d, %d]", p.X, p.Y)
+}
+
+func (p *Point) Draw() string {
+	var sb strings.Builder
+
+	for y := p.MaxY + 1; y >= p.MinY-1; y-- {
+		for x := p.MinX - 1; x <= p.MaxX+1; x++ {
+			_, ok := p.Visits[fmt.Sprintf("[%d, %d]", x, y)]
+			if x == 0 && y == 0 {
+				sb.WriteByte('o')
+			} else if ok {
+				sb.WriteByte('*')
+			} else {
+				sb.WriteByte('.')
+			}
+			sb.WriteByte(' ')
+		}
+		sb.WriteByte('\n')
+	}
+	return sb.String()
 }
