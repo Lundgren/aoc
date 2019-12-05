ff6dea3e981c4992a6059ae66de0027f5d11970b
Andreas Lundgren <andreasl@opay.team>
2019-12-05 08:17:14 +0100

    day5

--- golang/2019/day5/day5.go
+++ golang/2019/day5/day5.go
@@ -2,24 +2,14 @@ package day5
 
 import (
 	"aoc/aoc"
-	"fmt"
 	"strings"
 )
 
-var CorrectAnswers = []string{"", ""}
+var CorrectAnswers = []string{"13978427", "11189491"}
 
-var Examples = []aoc.Example{
-	// {Input: "3,0,4,0,99", Expected1: "0", Expected2: ""},
-	{Input: "1002,4,3,4,33", Expected1: "0", Expected2: ""},
-	// {Input: "", Expected1: "", Expected2: ""},
-	// {Input: "", Expected1: "", Expected2: ""},
-	// {Input: "", Expected1: "", Expected2: ""},
-}
+var Examples = []aoc.Example{}
 
 func Solve(in aoc.Input) (interface{}, interface{}) {
-
-	// part1, part2 := 0, 0
-
 	vals := strings.Split(in.String(), ",")
 	data := make([]int, len(vals))
 
@@ -27,44 +17,23 @@ func Solve(in aoc.Input) (interface{}, interface{}) {
 		data[i] = aoc.ParseInt(v)
 	}
 
-	part1 := run(data)
-	part1 = 0
-	// part2 := find(19690720, data)
-	part2 := 0
+	part1 := run(1, data)
+	part2 := run(5, data)
 
 	return part1, part2
 }
 
-func run(source []int) int {
+func run(input int, source []int) int {
 	data := append([]int(nil), source...)
-	// data[1], data[2] = noun, verb
-	pc := 0
-
-	input := 1 //??
-	output := 0
+	pc, output := 0, 0
 
 	for pc < len(data) {
-		v1, v2 := 0, 0
-		instr := data[pc]
-		if len(data) > pc+2 {
-			if (instr/100)%10 == 0 {
-				v1 = data[data[pc+1]]
-			} else {
-				v1 = data[pc+1]
-			}
-			if (instr/1000)%10 == 0 {
-				v2 = data[data[pc+2]]
-			} else {
-				v2 = data[pc+2]
-			}
+		instr, v1, v2 := getParams(pc, data)
+		if output != 0 && instr != 99 {
+			panic("Invalid state")
 		}
-		// fmt.Println("i", instr, (instr<<0)%10, (instr/10)%10, (instr/100)%10, (instr/1000)%10)
-		fmt.Println(">>", instr, "||", instr%100, v1, v2)
-		// if (instr<<5)%10 == 0 {
-		// 	v3 = data[v3]
-		// }
 
-		switch instr % 100 {
+		switch instr {
 		case 1:
 			data[data[pc+3]] = v1 + v2
 			pc += 4
@@ -73,33 +42,65 @@ func run(source []int) int {
 			pc += 4
 		case 3:
 			data[data[pc+1]] = input
-			fmt.Println(data)
 			pc += 2
 		case 4:
 			output = v1
 			pc += 2
-			fmt.Println("output:", output)
+		case 5:
+			if v1 != 0 {
+				pc = v2
+			} else {
+				pc += 3
+			}
+		case 6:
+			if v1 == 0 {
+				pc = v2
+			} else {
+				pc += 3
+			}
+		case 7:
+			if v1 < v2 {
+				data[data[pc+3]] = 1
+			} else {
+				data[data[pc+3]] = 0
+			}
+			pc += 4
+		case 8:
+			if v1 == v2 {
+				data[data[pc+3]] = 1
+			} else {
+				data[data[pc+3]] = 0
+			}
+			pc += 4
 		case 99:
-			fmt.Println("Finished on 99")
-			goto finished
+			return output
 		default:
-			pc++
+			panic("")
 		}
 	}
-finished:
 
-	fmt.Println(data)
-	return data[0]
-
+	return output
 }
 
-// func find(what int, data []int) int {
-// 	for n := 0; n <= 100; n++ {
-// 		for v := 0; v <= 100; v++ {
-// 			if run(n, v, data) == what {
-// 				return n*100 + v
-// 			}
-// 		}
-// 	}
-// 	panic("")
-// }
+func getParams(pc int, data []int) (instr, v1, v2 int) {
+	instr = data[pc] % 100
+	if instr == 99 {
+		return
+	}
+
+	if len(data) > pc+1 {
+		if (data[pc]/100)%10 == 0 {
+			v1 = data[data[pc+1]]
+		} else {
+			v1 = data[pc+1]
+		}
+	}
+	if len(data) > pc+2 && instr != 3 && instr != 4 {
+		if (data[pc]/1000)%10 == 0 {
+			v2 = data[data[pc+2]]
+		} else {
+			v2 = data[pc+2]
+		}
+	}
+	return
+}
