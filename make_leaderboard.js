e3c1214274d5c6bc61255f0fc6deb67a4b06b214
Andreas Lundgren <andreas@lundgren.io>
2024-12-05 20:15:05 +0100

    Added time to star 2 leaderboard

--- make_leaderboard.js
+++ make_leaderboard.js
@@ -35,12 +35,13 @@ for (const player of players) {
 
 let outputMd = "";
 
+const playerStar2Deltas = {};
 for (let day = 1; day <= currentDay; day++) {
   const startTime = day1StartTime + (day - 1) * 24 * 60 * 60;
 
   outputMd += `## Day ${day}\n`;
   outputMd += `| Rank | Name | Star 1 Time | Star 2 Time | Time After Winner | Star 1 -> Star 2 | Day Points | Total Points |\n`;
-  outputMd += `|------|------|------------|-------------|------------------|------------------|------------|-------------|\n`;
+  outputMd += `|------|------|-------------|-------------|-------------------|------------------|------------|--------------|\n`;
 
   const dayStats = [];
   for (const player of players) {
@@ -50,12 +51,23 @@ for (let day = 1; day <= currentDay; day++) {
     if (stars && stars["1"]) {
       const star1Time = stars["1"].get_star_ts - startTime;
       const star2Time = (stars["2"]?.get_star_ts || Infinity) - startTime;
+      const star2Delta = star2Time - star1Time;
+
+      if (star2Delta !== Infinity) {
+        playerStar2Deltas[name] = playerStar2Deltas[name] || {
+          name,
+          tot: 0,
+          deltas: [],
+        };
+        playerStar2Deltas[name].deltas.push(star2Delta);
+        playerStar2Deltas[name].tot += star2Delta;
+      }
 
       dayStats.push({
         name,
         star1Time,
         star2Time,
-        star2Delay: star2Time ? star2Time - star1Time : Infinity,
+        star2Delta,
       });
     }
   }
@@ -81,12 +93,40 @@ for (let day = 1; day <= currentDay; day++) {
       player.star1Time
     )} | ${getTime(player.star2Time)} | ${getTime(
       player.star1RelativeTime
-    )} | ${getTime(player.star2Delay)} | ${
+    )} | ${getTime(player.star2Delta)} | ${
       player.star1Points + player.star2Points
     } | ${playerPoints[player.name]} |\n`;
   }
 
   outputMd += `\n\n`;
 }
+
+outputMd += `## Star 2 Leaderboard\n`;
+outputMd += `| Rank | Name | Time to Star 2 | Time n-1 | Time n-2 |\n`;
+outputMd += `|------|------|----------------|----------|----------|\n`;
+
+const completedAll = Object.values(playerStar2Deltas).filter(
+  (p) => p.deltas.length == currentDay
+);
+completedAll.sort((a, b) => a.tot - b.tot);
+
+for (let i = 0; i < completedAll.length; i++) {
+  const p = completedAll[i];
+  p.deltas.sort((a, b) => a - b);
+
+  const gt = (arr) => {
+    const tot = arr.reduce((a, b) => a + b, 0);
+    return getTime(tot);
+  };
+
+  const t1 = gt(p.deltas);
+  const t2 = gt(p.deltas.slice(0, -1));
+  const t3 = gt(p.deltas.slice(0, -2));
+
+  outputMd += `| ${i + 1} | ${p.name} | ${t1} | ${t2} | ${t3} |\n`;
+}
+
+outputMd += `\n\n`;
+outputMd += `*Column 3 is the total time taken between star 1 & 2 independent of start time. Columns 4 & 5 shows the same with the worst 1 & 2 days removed from the sum.*`;
 
 fs.writeFileSync("leaderboard.md", outputMd);
